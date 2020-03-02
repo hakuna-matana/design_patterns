3c3a135b75766a5b64f8c7dad9d49314ac44c07c
anastasya.smir <anastasya.smir@gmail.com>
2020-03-02 21:57:03 +0300

    factory method examples

---
+++ FactoryMethod/FactoryMethod.js
@@ -0,0 +1,39 @@
+/**
+ * Factory Method(Фабричный метод) - шаблон, при котором создается класс(суперкласс),
+ * который позволяет создавать объекты(подклассы) на основании каких-либо входных данных.
+ *
+ * Используется, когда нужно создавать много однотипных объектов(одинаковая структура и разные данные).
+ * */
+
+/**
+ * Класс, который создает объект
+ * */
+class Bmw {
+  constructor(model, price, maxSpeed) {
+    this.model = model;
+    this.price = price;
+    this.maxSpeed = maxSpeed;
+  }
+}
+
+/**
+ * Фабрика - надстройка над классом
+ * */
+class BmwFactory {
+  create(type) {
+    if (type === 'X5') {
+      return new Bmw(type, 108000, 300)
+    }
+    if (type === 'X6') {
+      return new Bmw(type, 111000, 320)
+    }
+  }
+}
+
+const factory = new BmwFactory();
+
+const x5 = factory.create('X5');
+const x6 = factory.create('X6');
+
+console.log(x5); // Bmw { model: 'X5', price: 108000, maxSpeed: 300 }
+console.log(x6); // Bmw { model: 'X6', price: 111000, maxSpeed: 320 }
