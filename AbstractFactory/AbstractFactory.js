535bc048fca3214488073b34609a94860f5f8f04
anastasya.smir <anastasya.smir@gmail.com>
2020-03-02 22:20:41 +0300

    abstract factory example

---
+++ AbstractFactory/AbstractFactory.js
@@ -0,0 +1,39 @@
+/**
+ * Abstract Factory (Абстрактная фабрика) - паттерн, который создает интерфейс,
+ * группирующий другие фабрики, которые логически связаны друг с другом.
+ * То есть, абстракция для фабрики и фабричного метода.
+ * */
+
+/** Абстрактная фабрика */
+function bmwProducer(kind) {
+  return kind === 'sport' ? sportCarFactory : familyCarFactory;
+}
+
+/** Фабрики */
+
+function sportCarFactory() {
+  return new Z4();
+}
+
+function familyCarFactory() {
+  return new I3();
+}
+
+class Z4 {
+  info() {
+    return 'Z4 is a Sport car!'
+  }
+}
+
+class I3 {
+  info() {
+    return 'I3 is a Sport car!'
+  }
+}
+
+/** Инициализация Абстрактной фабрики спортивных автомобилей*/
+const produce = bmwProducer('sport');
+
+const myCar = produce();
+
+console.log(myCar.info())  // Z4 is a Sport car!
